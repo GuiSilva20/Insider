f75af149fd27f5db7a42de8a045254575190e840
GuiSilva20 <guilherme.nascimento6077@gmail.com>
2023-01-10 20:47:37 -0300

    Inicio

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Home from '../pages/home';
+import Movies from '../pages/movies';
+
+const Drawer = createDrawerNavigator();
+
+function Routes(){
+    return(
+        <Drawer.Navigator>
+            <Drawer.Screen name="Home" component={Home}/>
+            <Drawer.Screen name="Movies" component={Movies}/>
+
+        </Drawer.Navigator>
+    )
+}
+
+export default Routes
